7424c2cc8c2905ef1dbcc7ec9a0112bda252ee2b
farneser <farneser1@gmail.com>
2023-08-27 20:38:24 +0300

    feat: index page

---
+++ src/main/java/com/farneser/tennisscoreboard/NewMatchServlet.java
@@ -0,0 +1,24 @@
+package com.farneser.tennisscoreboard;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "new-match", value = "/new-match")
+public class NewMatchServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/new-match.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.sendRedirect("/matches");
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,11 +2,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>JSP - Hello World</title>
+    <title>JSP - Hello World</title>
 </head>
 <body>
-<h1><%= "Hello World!" %></h1>
-<br/>
-<a href="hello-servlet">Hello Servlet</a>
+<h1>Tennis scoreboard</h1>
+<div>
+    <a href="new-match">Create a new match</a>
+    <br>
+    <a href="#">Completed matches</a>
+</div>
+
 </body>
 </html>
