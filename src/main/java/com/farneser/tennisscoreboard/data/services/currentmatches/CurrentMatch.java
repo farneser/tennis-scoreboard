91e31c83efc7a6c17292cf1301bb731810c44bdb
farneser <farneser1@gmail.com>
2023-08-27 23:00:45 +0300

    feat(CurrentMatch): integer score

--- src/main/java/com/farneser/tennisscoreboard/data/services/currentmatches/CurrentMatch.java
+++ src/main/java/com/farneser/tennisscoreboard/data/services/currentmatches/CurrentMatch.java
@@ -1,6 +1,7 @@
 package com.farneser.tennisscoreboard.data.services.currentmatches;
 
 import com.farneser.tennisscoreboard.data.entities.Player;
+import com.farneser.tennisscoreboard.data.services.score.GamePoints;
 import lombok.Data;
 
 import java.util.UUID;
@@ -9,13 +10,19 @@ import java.util.UUID;
 public class CurrentMatch {
 
     private final UUID id;
+
     private final Player firstPlayer;
     private final Player secondPlayer;
     private Player winnerPlayer;
+
+    private int firstPlayerScore;
+    private int secondPlayerScore;
 
     public CurrentMatch(UUID id, Player firstPlayer, Player secondPlayer) {
         this.id = id;
         this.firstPlayer = firstPlayer;
         this.secondPlayer = secondPlayer;
+        firstPlayerScore = 0;
+        secondPlayerScore = 0;
     }
 }
