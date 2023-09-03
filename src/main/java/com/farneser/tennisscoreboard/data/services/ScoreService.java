9f65a0f95cbfdcfb3ed7622929bdbea3ce240c35
farneser <farneser1@gmail.com>
2023-09-04 00:16:54 +0300

    feat: score service with set calculator

--- src/main/java/com/farneser/tennisscoreboard/data/services/ScoreService.java
+++ src/main/java/com/farneser/tennisscoreboard/data/services/ScoreService.java
@@ -2,16 +2,27 @@ package com.farneser.tennisscoreboard.data.services;
 
 import com.farneser.tennisscoreboard.data.dto.WinnerType;
 import com.farneser.tennisscoreboard.data.services.currentmatches.CurrentMatch;
+import com.farneser.tennisscoreboard.data.services.score.State;
 import com.farneser.tennisscoreboard.data.services.score.calculator.GameCalculator;
+import com.farneser.tennisscoreboard.data.services.score.calculator.SetCalculator;
 
 public class ScoreService {
 
     public void process(CurrentMatch match, WinnerType winner) {
 
         var gameCalculator = new GameCalculator();
-        var result = gameCalculator.process(match, winner);
+        var setCalculator = new SetCalculator();
 
-        System.out.println(result);
+        var gameResult = gameCalculator.process(match, winner);
+        System.out.println("game " + match.getId() + " " + gameResult);
+
+        if (gameResult != State.GameInProcess) {
+
+            var setResult = setCalculator.process(match, winner);
+            System.out.println("set " + match.getId() + " " + setResult);
+        }
+
+
     }
 
 }
