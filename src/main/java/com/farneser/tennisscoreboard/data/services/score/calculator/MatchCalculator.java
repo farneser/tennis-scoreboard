7b1f48a4073a9ec07d8c20657ad44665ff7f1c4f
farneser <farneser1@gmail.com>
2023-09-04 00:47:19 +0300

    feat: match calculator implementation

---
+++ src/main/java/com/farneser/tennisscoreboard/data/services/score/calculator/MatchCalculator.java
@@ -0,0 +1,36 @@
+package com.farneser.tennisscoreboard.data.services.score.calculator;
+
+import com.farneser.tennisscoreboard.data.dto.WinnerType;
+import com.farneser.tennisscoreboard.data.services.currentmatches.CurrentMatch;
+import com.farneser.tennisscoreboard.data.services.score.State;
+
+public class MatchCalculator implements ICalculator {
+    @Override
+    public State process(CurrentMatch match, WinnerType winner) {
+
+        System.out.println(winner + " wins the set");
+
+        var setsWonCounter = 0;
+
+        for (var set : match.getSetScores()) {
+
+            if (set.getWinner(winner) > set.getLooser(winner)) {
+                setsWonCounter++;
+            }
+
+        }
+
+        if (match.getSetScores().size() >= match.winMatchesCount() &&
+                setsWonCounter >= match.winMatchesCount()) {
+
+            match.setWinnerPlayer(winner == WinnerType.FirstPlayer ?
+                    match.getFirstPlayer() :
+                    match.getSecondPlayer()
+            );
+
+            return WinnerType.convertToState(winner);
+        }
+
+        return State.GameInProcess;
+    }
+}
