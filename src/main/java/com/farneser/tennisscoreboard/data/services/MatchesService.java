c37f12b7f556774ea256f492a30dde34a3e74fb5
farneser <farneser1@gmail.com>
2023-09-09 17:05:40 +0300

    feat: matches service

---
+++ src/main/java/com/farneser/tennisscoreboard/data/services/MatchesService.java
@@ -0,0 +1,33 @@
+package com.farneser.tennisscoreboard.data.services;
+
+import com.farneser.tennisscoreboard.data.entities.Match;
+import com.farneser.tennisscoreboard.data.entities.dto.MatchesDto;
+import com.farneser.tennisscoreboard.data.entities.viewmodel.MatchListViewModel;
+import com.farneser.tennisscoreboard.data.services.hibernate.MatchService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatchesService {
+
+    private final MatchService matchService = new MatchService();
+
+    public MatchListViewModel persist(MatchesDto matchesDto) {
+        var pageSize = 10;
+        List<Match> matches = new ArrayList<>();
+
+        if (matchesDto.pageNumber() <= matchService.getLastPage(pageSize) && matchesDto.pageNumber() > 0) {
+            matches = matchService.get(
+                    matchesDto.pageNumber(),
+                    pageSize,
+                    matchesDto.searchText());
+        }
+
+        return new MatchListViewModel(
+                matches,
+                matchesDto.pageNumber(),
+                matchService.getLastPage(pageSize)
+        );
+    }
+
+}
